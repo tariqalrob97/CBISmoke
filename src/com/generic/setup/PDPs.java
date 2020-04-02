d596e7b1237f8bdef847fd2ec8d443701af57f53
tabualrob <tabualrob@mbarakat.IT.local>
2020-04-02 16:38:43 +0300

    Add to cart protection from out of stock PDPs

--- src/com/generic/setup/PDPs.java
+++ src/com/generic/setup/PDPs.java
@@ -5,6 +5,9 @@ import java.net.URISyntaxException;
 import java.util.ArrayList;
 import java.util.Random;
 
+import com.generic.selector.PDPSelectors;
+import com.generic.util.SelectorUtil;
+
 public class PDPs extends SelTestCase {
 	public static Random random = new Random();
 
@@ -18,10 +21,11 @@ public class PDPs extends SelTestCase {
 
 	static String[] RYproducts = new String[] {};
 
-	public static void navigateToRandomPDP() throws URISyntaxException {
+	public static void navigateToRandomPDP(int maxTries) throws URISyntaxException, InterruptedException {
 		getCurrentFunctionName(true);
 
 		String productURL;
+		maxTries--;
 		
 		ArrayList<String> brandProduct = (ArrayList<String>) products.get(getBrand());
 
@@ -29,10 +33,57 @@ public class PDPs extends SelTestCase {
 	
 
 		getDriver().get(new URI(getDriver().getCurrentUrl()).resolve(productURL).toString());
-
+		
+		Thread.sleep(2500);
+		
+		if((!checkifProductAvillable() || !checkifProductPuplished())&& maxTries>0) {
+			navigateToRandomPDP(maxTries);
+		}
+		
 		getCurrentFunctionName(false);
 
 	}//Navigate Method
+	
+	//Overload to give 5 as default
+	public static void navigateToRandomPDP() throws URISyntaxException, InterruptedException {
+		navigateToRandomPDP(5);
+	}
+
+	public static boolean checkifProductAvillable() {
+		getCurrentFunctionName(true);
+		boolean state = true;
+		try {
+			if (SelectorUtil.isDisplayed(PDPSelectors.Unavilable_Product_Message.get()))
+				state = false;
+
+			getCurrentFunctionName(false);
+			return state;
+		} catch (Exception e) {
+			getCurrentFunctionName(false);
+			state = true;
+			return state;
+		}
+
+	}// Check Method
+	
+	public static boolean checkifProductPuplished() {
+		getCurrentFunctionName(true);
+		boolean state = true;
+		try {
+			if (SelectorUtil.isDisplayed(PDPSelectors.UnPuplished_Product_Message.get()))
+				state = false;
+
+			getCurrentFunctionName(false);
+			return state;
+		} catch (Exception e) {
+			getCurrentFunctionName(false);
+			state = true;
+			return state;
+		}
+
+	}// Check Method
+	
+	
 
 
 }//Class
